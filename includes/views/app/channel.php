d35f6f76078964b8ad2635375a015ab3e92f151b
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2026-03-08 18:28:38 -0500

    --

--- includes/views/app/channel.php
+++ includes/views/app/channel.php
@@ -20,6 +20,53 @@ $relStatusMap = ['single' => 'Soltero/a', 'married' => 'Casado/a', 'in_a_relatio
 $interestedInMap = ['men' => 'Hombres', 'women' => 'Mujeres', 'both' => 'Hombres y Mujeres', 'other' => 'Otro', '' => 'No especificado'];
 $genderMap = ['male' => 'Hombre', 'female' => 'Mujer', 'non-binary' => 'No binario', 'other' => 'Otro', '' => 'No especificado'];
 $hairColorMap = ['black' => 'Negro', 'brown' => 'Castaño', 'blonde' => 'Rubio', 'red' => 'Pelirrojo', 'other' => 'Otro', '' => 'No especificado'];
+$boobsMap = ['natural' => 'Naturales', 'augmented' => 'Operados/Aumentados', '' => 'No especificado'];
+$ethnicityMap = ['latina' => 'Latina', 'caucasian' => 'Caucásica/Blanca', 'asian' => 'Asiática', 'black' => 'Afrodescendiente/Negra', 'mixed' => 'Mixta', 'indigenous' => 'Indígena', '' => 'No especificado'];
+$eyeColorMap = ['brown' => 'Café', 'blue' => 'Azul', 'green' => 'Verde', 'hazel' => 'Miel/Avellana', 'black' => 'Negros', '' => 'No especificado'];
+
+// Lista completa de países
+$countriesMap = [
+    'AF' => 'Afganistán', 'AL' => 'Albania', 'DE' => 'Alemania', 'AD' => 'Andorra', 'AO' => 'Angola', 
+    'AG' => 'Antigua y Barbuda', 'SA' => 'Arabia Saudita', 'DZ' => 'Argelia', 'AR' => 'Argentina', 
+    'AM' => 'Armenia', 'AU' => 'Australia', 'AT' => 'Austria', 'AZ' => 'Azerbaiyán', 'BS' => 'Bahamas', 
+    'BD' => 'Bangladés', 'BB' => 'Barbados', 'BH' => 'Baréin', 'BE' => 'Bélgica', 'BZ' => 'Belice', 
+    'BJ' => 'Benín', 'BY' => 'Bielorrusia', 'MM' => 'Birmania', 'BO' => 'Bolivia', 'BA' => 'Bosnia y Herzegovina', 
+    'BW' => 'Botsuana', 'BR' => 'Brasil', 'BN' => 'Brunéi', 'BG' => 'Bulgaria', 'BF' => 'Burkina Faso', 
+    'BI' => 'Burundi', 'BT' => 'Bután', 'CV' => 'Cabo Verde', 'KH' => 'Camboya', 'CM' => 'Camerún', 
+    'CA' => 'Canadá', 'QA' => 'Catar', 'TD' => 'Chad', 'CL' => 'Chile', 'CN' => 'China', 'CY' => 'Chipre', 
+    'VA' => 'Ciudad del Vaticano', 'CO' => 'Colombia', 'KM' => 'Comoras', 'KP' => 'Corea del Norte', 
+    'KR' => 'Corea del Sur', 'CI' => 'Costa de Marfil', 'CR' => 'Costa Rica', 'HR' => 'Croacia', 'CU' => 'Cuba', 
+    'DK' => 'Dinamarca', 'DM' => 'Dominica', 'EC' => 'Ecuador', 'EG' => 'Egipto', 'SV' => 'El Salvador', 
+    'AE' => 'Emiratos Árabes Unidos', 'ER' => 'Eritrea', 'SK' => 'Eslovaquia', 'SI' => 'Eslovenia', 
+    'ES' => 'España', 'US' => 'Estados Unidos', 'EE' => 'Estonia', 'ET' => 'Etiopía', 'PH' => 'Filipinas', 
+    'FI' => 'Finlandia', 'FJ' => 'Fiyi', 'FR' => 'Francia', 'GA' => 'Gabón', 'GM' => 'Gambia', 'GE' => 'Georgia', 
+    'GH' => 'Ghana', 'GD' => 'Granada', 'GR' => 'Grecia', 'GT' => 'Guatemala', 'GY' => 'Guyana', 'GN' => 'Guinea', 
+    'GQ' => 'Guinea Ecuatorial', 'GW' => 'Guinea-Bisáu', 'HT' => 'Haití', 'HN' => 'Honduras', 'HU' => 'Hungría', 
+    'IN' => 'India', 'ID' => 'Indonesia', 'IQ' => 'Irak', 'IR' => 'Irán', 'IE' => 'Irlanda', 'IS' => 'Islandia', 
+    'MH' => 'Islas Marshall', 'SB' => 'Islas Salomón', 'IL' => 'Israel', 'IT' => 'Italia', 'JM' => 'Jamaica', 
+    'JP' => 'Japón', 'JO' => 'Jordania', 'KZ' => 'Kazajistán', 'KE' => 'Kenia', 'KG' => 'Kirguistán', 
+    'KI' => 'Kiribati', 'KW' => 'Kuwait', 'LA' => 'Laos', 'LS' => 'Lesoto', 'LV' => 'Letonia', 'LB' => 'Líbano', 
+    'LR' => 'Liberia', 'LY' => 'Libia', 'LI' => 'Liechtenstein', 'LT' => 'Lituania', 'LU' => 'Luxemburgo', 
+    'MG' => 'Madagascar', 'MY' => 'Malasia', 'MW' => 'Malaui', 'MV' => 'Maldivas', 'ML' => 'Malí', 
+    'MT' => 'Malta', 'MA' => 'Marruecos', 'MU' => 'Mauricio', 'MR' => 'Mauritania', 'MX' => 'México', 
+    'FM' => 'Micronesia', 'MD' => 'Moldavia', 'MC' => 'Mónaco', 'MN' => 'Mongolia', 'ME' => 'Montenegro', 
+    'MZ' => 'Mozambique', 'NA' => 'Namibia', 'NR' => 'Nauru', 'NP' => 'Nepal', 'NI' => 'Nicaragua', 
+    'NE' => 'Níger', 'NG' => 'Nigeria', 'NO' => 'Noruega', 'NZ' => 'Nueva Zelanda', 'OM' => 'Omán', 
+    'NL' => 'Países Bajos', 'PK' => 'Pakistán', 'PW' => 'Palaos', 'PA' => 'Panamá', 'PG' => 'Papúa Nueva Guinea', 
+    'PY' => 'Paraguay', 'PE' => 'Perú', 'PL' => 'Polonia', 'PT' => 'Portugal', 'GB' => 'Reino Unido', 
+    'CF' => 'República Centroafricana', 'CZ' => 'República Checa', 'MK' => 'Macedonia del Norte', 
+    'CG' => 'República del Congo', 'CD' => 'República Democrática del Congo', 'DO' => 'República Dominicana', 
+    'ZA' => 'Sudáfrica', 'RW' => 'Ruanda', 'RO' => 'Rumanía', 'RU' => 'Rusia', 'WS' => 'Samoa', 
+    'KN' => 'San Cristóbal y Nieves', 'SM' => 'San Marino', 'VC' => 'San Vicente y las Granadinas', 
+    'LC' => 'Santa Lucía', 'ST' => 'Santo Tomé y Príncipe', 'SN' => 'Senegal', 'RS' => 'Serbia', 'SC' => 'Seychelles', 
+    'SL' => 'Sierra Leona', 'SG' => 'Singapur', 'SY' => 'Siria', 'SO' => 'Somalia', 'LK' => 'Sri Lanka', 
+    'SZ' => 'Suazilandia', 'SD' => 'Sudán', 'SS' => 'Sudán del Sur', 'SE' => 'Suecia', 'CH' => 'Suiza', 
+    'SR' => 'Surinam', 'TH' => 'Tailandia', 'TZ' => 'Tanzania', 'TJ' => 'Tayikistán', 'TL' => 'Timor Oriental', 
+    'TG' => 'Togo', 'TO' => 'Tonga', 'TT' => 'Trinidad y Tobago', 'TN' => 'Túnez', 'TM' => 'Turkmenistán', 
+    'TR' => 'Turquía', 'TV' => 'Tuvalu', 'UA' => 'Ucrania', 'UG' => 'Uganda', 'UY' => 'Uruguay', 'UZ' => 'Uzbekistán', 
+    'VU' => 'Vanuatu', 'VE' => 'Venezuela', 'VN' => 'Vietnam', 'YE' => 'Yemen', 'DJ' => 'Yibuti', 'ZM' => 'Zambia', 
+    'ZW' => 'Zimbabue', '' => 'No especificado'
+];
 
 if (!function_exists('time_elapsed_string')) {
     function time_elapsed_string($datetime, $full = false) {
@@ -352,7 +399,11 @@ $channelContact = $channelExists ? ($channelUser['channel_contact_email'] ?? '')
                                     'Estado de relación' => $relStatusMap[$channelUser['relationship_status'] ?? ''] ?? 'No especificado',
                                     'Interesado en' => $interestedInMap[$channelUser['interested_in'] ?? ''] ?? 'No especificado',
                                     'Género' => $genderMap[$channelUser['gender'] ?? ''] ?? 'No especificado',
+                                    'País de nacimiento' => $countriesMap[$channelUser['country'] ?? ''] ?? 'No especificado',
+                                    'Etnia / Descendencia' => $ethnicityMap[$channelUser['ethnicity'] ?? ''] ?? 'No especificado',
+                                    'Color de ojos' => $eyeColorMap[$channelUser['eye_color'] ?? ''] ?? 'No especificado',
                                     'Color de cabello' => $hairColorMap[$channelUser['hair_color'] ?? ''] ?? 'No especificado',
+                                    'Pechos / Busto' => $boobsMap[$channelUser['boobs'] ?? ''] ?? 'No especificado',
                                     'Estatura' => !empty($channelUser['height']) ? htmlspecialchars($channelUser['height']) . ' m' : 'No especificado',
                                     'Peso' => !empty($channelUser['weight']) ? htmlspecialchars($channelUser['weight']) . ' kg' : 'No especificado',
                                     'Tatuajes' => !empty($channelUser['tattoos']) ? 'Sí' : 'No',
