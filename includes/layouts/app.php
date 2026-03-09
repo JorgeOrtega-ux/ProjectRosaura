9d25f4efae89de92a26273f4e8ac6e59bd5153aa
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2026-03-09 11:31:49 -0500

    --

--- includes/layouts/app.php
+++ includes/layouts/app.php
@@ -20,9 +20,17 @@ $topBarClass = ($isAuthRoute || $isMaintenanceRestricted) ? 'disabled' : '';
     
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" />
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    
     <link rel="stylesheet" type="text/css" href="assets/css/components/components.css">
-    <script src="https://unpkg.com/hls.js@latest/dist/hls.min.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="assets/css/components/components-admin.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/components/components-auth.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/components/components-settings.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/components/components-channel.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/components/components-watch.css">
     <link rel="stylesheet" type="text/css" href="assets/css/components/components-studio.css">
+    
+    <script src="https://unpkg.com/hls.js@latest/dist/hls.min.js"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/root.css">
     <title>Project Rosaura</title>
     
