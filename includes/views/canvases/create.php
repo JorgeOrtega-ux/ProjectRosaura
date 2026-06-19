afe1c55ee5319cc8f0ecc119366f102c67d17951
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2026-06-18 21:00:05 -0500

    --

--- includes/views/canvases/create.php
+++ includes/views/canvases/create.php
@@ -1,16 +1,188 @@
 <?php // includes/views/canvases/create.php ?>
-<div class="view-content">
-    <div class="component-wrapper component-wrapper--full no-padding" data-ref="purchase-history-wrapper">
-        <div class="component-top">
-            <div class="component-top-left">
-                <h1 class="component-top-title">Crear nuevo lienzo</h1>
-            </div>
-            <div class="component-top-right">
-                <div class="component-actions active">
-                </div>
-            </div>
+<div class="view-content" data-ref="canvas-create-wrapper">
+    
+    <div class="component-top">
+        <div class="component-top-left">
+            <h1 class="component-top-title">canvas_create_title</h1>
         </div>
-        <div class="component-bottom">
+        <div class="component-top-right">
+            <button type="button" class="component-button component-button--icon component-button--h40" data-action="createCanvas" data-tooltip="btn_create_canvas" data-position="bottom">
+                <span class="material-symbols-rounded">add_box</span>
+            </button>
+        </div>
+    </div>
+
+    <div class="component-viewport">
+        <div class="component-wrapper">
+            <div class="component-bottom">
+                
+                <div class="component-card--grouped">
+
+                    <div class="component-group-item component-group-item--stateful">
+                        
+                        <div class="active component-state-box" data-state="canvasname-view">
+                            <div class="component-card__content">
+                                <div class="component-card__text">
+                                    <h2 class="component-card__title">canvas_name_title</h2>
+                                    <span class="component-display-value" data-ref="display-canvasname">Cargando...</span>
+                                </div>
+                            </div>
+                            <div class="component-card__actions component-card__actions--stretch">
+                                <button type="button" class="component-button component-button--h34" data-action="toggleEditState" data-target="canvasname">btn_edit</button>
+                            </div>
+                        </div>
+
+                        <div class="disabled component-state-box" data-state="canvasname-edit">
+                            <div class="component-card__content">
+                                <div class="component-card__text">
+                                    <h2 class="component-card__title">canvas_name_title</h2>
+                                    <div class="component-edit-row">
+                                        <div class="component-input-group component-input-group--h34">
+                                            <input type="text" data-ref="input-canvasname" class="component-input-field component-input-field--simple" value="" data-original-value="" placeholder="ph_canvas_name">
+                                        </div>
+                                        <div class="component-card__actions component-card__actions--stretch">
+                                            <button type="button" class="component-button component-button--h34" data-action="toggleEditState" data-target="canvasname">btn_cancel</button>
+                                            <button type="button" class="component-button component-button--h34 component-button--dark" data-action="saveCanvasName">btn_save</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="component-divider">
+
+                    <div class="component-group-item component-group-item--stacked">
+                        <div class="component-card__content">
+                            <div class="component-card__text">
+                                <h2 class="component-card__title">canvas_desc_title</h2>
+                                <p class="component-card__description">canvas_desc_desc</p>
+                            </div>
+                        </div>
+                        <div class="component-card__actions component-card__actions--start">
+                            <div class="component-input-group component-input-group--h34">
+                                <input type="text" data-ref="input-canvas-desc" class="component-input-field component-input-field--simple" placeholder="ph_canvas_desc">
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="component-divider">
+
+                    <div class="component-group-item component-group-item--stacked">
+                        <div class="component-card__content">
+                            <div class="component-card__text">
+                                <h2 class="component-card__title">canvas_size_title</h2>
+                                <p class="component-card__description">canvas_size_desc</p>
+                            </div>
+                        </div>
+                        <div class="component-card__actions component-card__actions--start">
+                            <div class="component-dropdown-wrapper">
+                                <div class="component-dropdown-trigger" data-action="toggleDropdown" data-target="dropdownSize">
+                                    <span class="material-symbols-rounded">crop_square</span>
+                                    <span class="component-dropdown-text" data-ref="text-size">64x64</span>
+                                    <span class="material-symbols-rounded">expand_more</span>
+                                </div>
+                                <div class="component-module component-module--dropdown component-module--dropdown-left disabled" data-module="dropdownSize">
+                                    <div class="component-menu component-menu--w-full component-menu--h-auto component-menu--no-padding component-menu--limited">
+                                        <div class="pill-container"><div class="drag-handle"></div></div>
+                                        <div class="component-menu-list component-menu-list--scrollable">
+                                            <div class="component-menu-link active" data-action="selectValue" data-type="size" data-value="64" data-label="64x64" data-icon="crop_square">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">crop_square</span></div>
+                                                <div class="component-menu-link-text"><span>64x64</span></div>
+                                            </div>
+                                            <div class="component-menu-link" data-action="selectValue" data-type="size" data-value="128" data-label="128x128" data-icon="aspect_ratio">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">aspect_ratio</span></div>
+                                                <div class="component-menu-link-text"><span>128x128</span></div>
+                                            </div>
+                                            <div class="component-menu-link" data-action="selectValue" data-type="size" data-value="264" data-label="264x264" data-icon="grid_4x4">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">grid_4x4</span></div>
+                                                <div class="component-menu-link-text"><span>264x264</span></div>
+                                            </div>
+                                            <div class="component-menu-link" data-action="selectValue" data-type="size" data-value="512" data-label="512x512" data-icon="grid_on">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">grid_on</span></div>
+                                                <div class="component-menu-link-text"><span>512x512</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="component-divider">
+
+                    <div class="component-group-item component-group-item--stacked">
+                        <div class="component-card__content">
+                            <div class="component-card__text">
+                                <h2 class="component-card__title">canvas_privacy_title</h2>
+                                <p class="component-card__description">canvas_privacy_desc</p>
+                            </div>
+                        </div>
+                        <div class="component-card__actions component-card__actions--start">
+                            <div class="component-dropdown-wrapper">
+                                <div class="component-dropdown-trigger" data-action="toggleDropdown" data-target="dropdownPrivacy">
+                                    <span class="material-symbols-rounded">public</span>
+                                    <span class="component-dropdown-text" data-ref="text-privacy">canvas_privacy_public</span>
+                                    <span class="material-symbols-rounded">expand_more</span>
+                                </div>
+                                <div class="component-module component-module--dropdown component-module--dropdown-left disabled" data-module="dropdownPrivacy">
+                                    <div class="component-menu component-menu--w-full component-menu--h-auto component-menu--no-padding component-menu--limited">
+                                        <div class="pill-container"><div class="drag-handle"></div></div>
+                                        <div class="component-menu-list component-menu-list--scrollable">
+                                            <div class="component-menu-link active" data-action="selectValue" data-type="privacy" data-value="public" data-label="canvas_privacy_public" data-icon="public">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">public</span></div>
+                                                <div class="component-menu-link-text"><span>canvas_privacy_public</span></div>
+                                            </div>
+                                            <div class="component-menu-link" data-action="selectValue" data-type="privacy" data-value="unlisted" data-label="canvas_privacy_unlisted" data-icon="link">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">link</span></div>
+                                                <div class="component-menu-link-text"><span>canvas_privacy_unlisted</span></div>
+                                            </div>
+                                            <div class="component-menu-link" data-action="selectValue" data-type="privacy" data-value="private" data-label="canvas_privacy_private" data-icon="lock">
+                                                <div class="component-menu-link-icon"><span class="material-symbols-rounded">lock</span></div>
+                                                <div class="component-menu-link-text"><span>canvas_privacy_private</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="component-divider">
+
+                    <div class="component-group-item component-group-item--stacked">
+                        <div class="component-card__content">
+                            <div class="component-card__text">
+                                <h2 class="component-card__title">canvas_limit_title</h2>
+                                <p class="component-card__description">canvas_limit_desc</p>
+                            </div>
+                        </div>
+                        <div class="component-card__actions component-card__actions--start">
+                            <div class="component-inline-control component-inline-control--fixed">
+                                <div class="component-inline-control__group">
+                                    <button type="button" class="component-inline-control__btn" data-action="adjustLimit" data-step="-50" data-min="10">
+                                        <span class="material-symbols-rounded">keyboard_double_arrow_left</span>
+                                    </button>
+                                    <button type="button" class="component-inline-control__btn" data-action="adjustLimit" data-step="-10" data-min="10">
+                                        <span class="material-symbols-rounded">chevron_left</span>
+                                    </button>
+                                </div>
+                                <div class="component-inline-control__center" data-ref="val_limit" data-val="10">10</div>
+                                <div class="component-inline-control__group">
+                                    <button type="button" class="component-inline-control__btn" data-action="adjustLimit" data-step="10" data-max="50000">
+                                        <span class="material-symbols-rounded">chevron_right</span>
+                                    </button>
+                                    <button type="button" class="component-inline-control__btn" data-action="adjustLimit" data-step="50" data-max="50000">
+                                        <span class="material-symbols-rounded">keyboard_double_arrow_right</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
--- public/assets/js/modules/canvases/CanvasesController.js
+++ public/assets/js/modules/canvases/CanvasesController.js
@@ -3,15 +3,156 @@
 export class CanvasesController {
     constructor() {
         this.name = "CanvasesController";
+        this.container = null;
+        
+        // Estado centralizado para el payload de creación
+        this.formState = {
+            name: '',
+            description: '',
+            size: '64', // Valor por defecto
+            privacy: 'public',
+            limit: 10 // Mínimo inicial
+        };
+
+        this.handleAction = this.handleAction.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
     }
 
     async init() {
         console.log(`${this.name} initialized`);
-        // Lógica de inicio y listeners para las pantallas de lienzos...
+        this.container = document.querySelector('[data-ref="canvas-create-wrapper"]');
+        
+        if (this.container) {
+            this.container.addEventListener('click', this.handleAction);
+            document.addEventListener('click', this.handleClickOutside);
+            
+            // Inyectamos el nombre por defecto con timestamp
+            this.setupDefaultValues();
+        }
     }
 
     destroy() {
         console.log(`${this.name} destroyed`);
-        // Limpieza de eventos...
+        if (this.container) {
+            this.container.removeEventListener('click', this.handleAction);
+        }
+        document.removeEventListener('click', this.handleClickOutside);
+    }
+
+    setupDefaultValues() {
+        const timestampName = `Canvas_${Date.now()}`;
+        this.formState.name = timestampName;
+
+        const displayEl = this.container.querySelector('[data-ref="display-canvasname"]');
+        const inputEl = this.container.querySelector('[data-ref="input-canvasname"]');
+
+        if (displayEl) displayEl.textContent = timestampName;
+        if (inputEl) {
+            inputEl.value = timestampName;
+            inputEl.setAttribute('data-original-value', timestampName);
+        }
+    }
+
+    handleAction(e) {
+        const actionBtn = e.target.closest('[data-action]');
+        if (!actionBtn) return;
+
+        const action = actionBtn.getAttribute('data-action');
+
+        switch (action) {
+            case 'toggleDropdown':
+                this.toggleDropdown(actionBtn);
+                break;
+            case 'selectValue':
+                this.selectDropdownValue(actionBtn);
+                break;
+            case 'adjustLimit':
+                this.adjustParticipantLimit(actionBtn);
+                break;
+            case 'createCanvas':
+                this.submitCanvas();
+                break;
+        }
+    }
+
+    toggleDropdown(triggerBtn) {
+        const targetId = triggerBtn.getAttribute('data-target');
+        const targetDropdown = document.querySelector(`[data-module="${targetId}"]`);
+        
+        // Cerrar otros dropdowns
+        document.querySelectorAll('.component-module--dropdown:not(.disabled)').forEach(el => {
+            if (el !== targetDropdown) el.classList.add('disabled');
+        });
+
+        if (targetDropdown) {
+            targetDropdown.classList.toggle('disabled');
+        }
+    }
+
+    selectDropdownValue(optionBtn) {
+        const type = optionBtn.getAttribute('data-type');
+        const value = optionBtn.getAttribute('data-value');
+        const label = optionBtn.getAttribute('data-label');
+        const icon = optionBtn.getAttribute('data-icon');
+
+        this.formState[type] = value;
+
+        // Actualizar visualmente la opción activa
+        const menu = optionBtn.closest('.component-menu-list');
+        menu.querySelectorAll('.component-menu-link').forEach(el => el.classList.remove('active'));
+        optionBtn.classList.add('active');
+
+        // Actualizar el trigger
+        const dropdownWrapper = optionBtn.closest('.component-dropdown-wrapper');
+        const triggerText = dropdownWrapper.querySelector('.component-dropdown-text');
+        
+        if (triggerText) triggerText.textContent = label;
+
+        if (icon) {
+            const triggerIcon = dropdownWrapper.querySelector('.component-dropdown-trigger .material-symbols-rounded:first-child');
+            if (triggerIcon) triggerIcon.textContent = icon;
+        }
+
+        // Cerrar módulo
+        const module = dropdownWrapper.querySelector('.component-module--dropdown');
+        if (module) module.classList.add('disabled');
+    }
+
+    adjustParticipantLimit(btn) {
+        const step = parseInt(btn.getAttribute('data-step'), 10);
+        const min = parseInt(btn.getAttribute('data-min'), 10) || 10;
+        const max = parseInt(btn.getAttribute('data-max'), 10) || 50000;
+        
+        const centerElement = this.container.querySelector('[data-ref="val_limit"]');
+        let currentVal = parseInt(centerElement.getAttribute('data-val'), 10) || min;
+
+        let newVal = currentVal + step;
+        
+        if (newVal < min) newVal = min;
+        if (newVal > max) newVal = max;
+
+        this.formState.limit = newVal;
+        centerElement.setAttribute('data-val', newVal);
+        centerElement.textContent = newVal;
+    }
+
+    submitCanvas() {
+        // Recuperar el valor final de los inputs antes de enviar
+        const inputName = this.container.querySelector('[data-ref="input-canvasname"]');
+        const inputDesc = this.container.querySelector('[data-ref="input-canvas-desc"]');
+
+        this.formState.name = inputName ? inputName.value.trim() : this.formState.name;
+        this.formState.description = inputDesc ? inputDesc.value.trim() : '';
+
+        console.log("Creando lienzo con el payload:", this.formState);
+        // ApiServices.post('/canvases', this.formState).then(...)
+    }
+
+    handleClickOutside(e) {
+        if (!e.target.closest('.component-dropdown-wrapper')) {
+            document.querySelectorAll('.component-module--dropdown:not(.disabled)').forEach(el => {
+                el.classList.add('disabled');
+            });
+        }
     }
 }
